103e1f511edbf86dba0646e021c6bbd74a5ff44f
Thomas Pfeiffer <totothekiller@gmail.com>
2013-11-12 22:43:57 +0100

    Refactor TX side
    -> Remove ReadTempLib
    -> support for ATtiny and Uno
    
    Use PCREL Patch for WinAVR from :
    - http://forum.arduino.cc/index.php?topic=168510.0
    - https://github.com/TCWORLD/ATTinyCore/tree/master/PCREL%20Patch%20for%20GCC

--- transmit/transmit.ino
+++ transmit/transmit.ino
@@ -1,8 +1,8 @@
+//
 // TX Sensor
-
+//
 
 // Info : 
-
 // Snootlab : http://forum.snootlab.com/viewtopic.php?f=38&t=399
 // Virtual Wire library : http://www.airspayce.com/mikem/arduino/VirtualWire/VirtualWire_8h.html
 // http://www.pjrc.com/teensy/td_libs_VirtualWire.html
@@ -10,12 +10,24 @@
 
 #include <VirtualWire.h>
 #include <OneWire.h>
-#include "ReadTempLib.h"
 
-int txLed = 13; // TX LED
-int txPin = 12; // TX Out
-int sensPin = 7; // Sensor Pin
-uint8_t sensID = 42; // Sensor ID
+#define  ATtiny85 // ATtiny85 mode
+
+#ifdef ATtiny85
+  #define txLed 3  // TX LED
+  #define txPin 1  // TX Out
+  #define sensPin 0  // Sensor Pin
+#else
+  #define DEBUG  // Is serial debug active ?
+  
+  #define txLed 13  // TX LED
+  #define txPin 12  // TX Out
+  #define sensPin 7  // Sensor Pin
+#endif
+
+#define sensID 42  // Sensor ID
+
+#define SENSOR_UPDATE  5000 // Delay
 
 // Sensor Message Structure
 typedef struct sensorData_t{
@@ -29,26 +41,28 @@ typedef union sensorData_union_t{
 };
 
 // Sensor Message
-sensorData_union_t message; 
+sensorData_union_t _message; 
 
-// Sensor Library
-ReadTempLib sensor(sensPin);
+// OneWire protocol
+OneWire _wire(sensPin);
 
 void setup()
 {
   // Debug
+  #ifdef DEBUG
   Serial.begin(9600);
-  Serial.println("setup");
-  
+  Serial.println("--- SETUP ---");
+  #endif
+
   // Led Setup
   pinMode(txLed, OUTPUT);
 
   // TX Setup
   vw_set_tx_pin(txPin);
   vw_setup(2000);
-  
+
   // Write Sensor ID
-  message.data.id = sensID;
+  _message.data.id = sensID;
 }
 
 void loop()
@@ -56,21 +70,14 @@ void loop()
   digitalWrite(txLed, HIGH);
 
   // Read Temp
-  if ( sensor.acquireTemp())
+  if (acquireTemp())
   {  
-    // set Temperature into message
-    message.data.value=sensor.getTemp();
-    
-    if(Serial)
-    {
-      Serial.print("Sensor=");
-      Serial.print(message.data.id);
-      Serial.print(", Temp=");
-      Serial.println(message.data.value);
-    }
-    
+    #ifdef DEBUG
+    Serial.print("Sensor="); Serial.print(_message.data.id); Serial.print(", Temp="); Serial.println(_message.data.value);
+    #endif
+
     // Send
-    vw_send(message.raw, 5);
+    vw_send(_message.raw, 5);
     vw_wait_tx(); // Wait until the whole message is gone
   }
 
@@ -78,11 +85,73 @@ void loop()
   digitalWrite(txLed, LOW);    // sets the LED off
 
   // Sleep
-  delay(5000);
+  delay(SENSOR_UPDATE);
 }
 
 
+/*
+*
+* Read Temp
+*
+*/
+boolean acquireTemp()
+{
+  byte i;
+  byte data[12];
+  byte addr[8];
 
+  // Reset Search
+  _wire.reset_search();
+
+  // Get first sensor
+  if ( !_wire.search(addr)) {
+    #ifdef DEBUG
+    Serial.println("No more addresses.");
+    #endif
+
+    return false;
+  }
+
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+    #ifdef DEBUG
+    Serial.println("CRC is not valid!");
+    #endif
+
+    return false;
+  }
+
+  // Check if chip is DS18B20
+  if(addr[0]!=0x28)
+  {
+    #ifdef DEBUG
+    Serial.println("Chip is not DS18B20");
+    #endif
+
+    return false;
+  }
+
+  _wire.reset();
+  _wire.select(addr);
+  _wire.write(0x44,1);// start conversion
+
+  delay(1000);     // maybe 750ms is enough, maybe not
+
+  _wire.reset();
+  _wire.select(addr);    
+  _wire.write(0xBE);// Read Scratchpad
+
+  for ( i = 0; i < 9; i++) {// we need 9 bytes
+    data[i] = _wire.read();
+  }
+
+  // Convert the data to actual temperature
+  float raw = (data[1] << 8) | data[0];
+
+  // Save temperature
+  _message.data.value = raw / 16.0;
+
+  return true;
+}
 
 
 
