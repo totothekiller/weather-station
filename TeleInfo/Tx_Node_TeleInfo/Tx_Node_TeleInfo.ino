3c866d6174bce84fcf47037eb6e82f1cca74b06a
Thomas Pfeiffer <totothekiller@gmail.com>
2014-01-12 15:43:40 +0100

    Add TX + ATtiny85 support

--- TeleInfo/Tx_Node_TeleInfo/Tx_Node_TeleInfo.ino
+++ TeleInfo/Tx_Node_TeleInfo/Tx_Node_TeleInfo.ino
@@ -6,10 +6,42 @@
  */
 
 #include <SoftwareSerial.h>
+#include <VirtualWire.h>
+#include <avr/wdt.h>
+#include <avr/power.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
 
+
+#undef  ATtiny85 // ATtiny85 mode
+
+#ifdef ATtiny85
+#define ECO // Low Power Mode
 
 #define rxSerialPin 2  // RX PIN for Software Serial
 #define txSerialPin 3  // TX PIN for Software Serial
+
+#define txLed 3  // TX LED
+#define txPin 1  // TX Out
+#define powerPin 4 // Power line for Tx module and sensor
+
+#else
+#define DEBUG  // Is serial debug active ?
+
+#define rxSerialPin 2  // RX PIN for Software Serial
+#define txSerialPin 3  // TX PIN for Software Serial
+
+#define txLed 13  // TX LED
+#define txPin 12  // TX Out
+#define powerPin 14 // Power line for Tx module and sensor
+
+#endif
+
+#define WATCHDOG_TIMER WDTO_8S // 8 sec
+#define SLEEP_LOOP  2 // Deep Sleep total time = WATCHDOG_TIMER * SLEEP_LOOP
+
+#define sensID 5  // Sensor ID
+
 
 // Sensor Message Structure
 typedef struct sensorData_t{
@@ -25,19 +57,63 @@ typedef union sensorData_union_t{
 // Sensor Message
 sensorData_union_t _message; 
 
+// EDF Link
 SoftwareSerial _edfLine(rxSerialPin, txSerialPin); // rx, tx pins
 
-void setup() {
-  Serial.begin(57600);
+// WatchDof Counter
+volatile byte _watchdogCounter;
 
-  _edfLine.begin(1200);
-
-  Serial.println("---- Hello ----");
+// WatchDog interruption
+ISR(WDT_vect) {
+  _watchdogCounter++;
 }
 
-void loop() {
-  // Read Apparent Power
-  acquireApparentPower();
+void setup() {
+  
+    // Debug
+#ifdef DEBUG
+  Serial.begin(57600);
+  Serial.println("--- SETUP ---");
+#endif
+
+  // Start Serial with EDF box
+  _edfLine.begin(1200);
+  
+  // Write Sensor ID
+  _message.data.id = sensID;
+}
+
+void loop()
+{
+  // Power On !
+  powerUp();
+
+  // Acquire Apparent Power
+  if (acquireApparentPower())
+  {  
+    // Turn LED On
+    digitalWrite(txLed, HIGH);
+
+#ifdef DEBUG
+    Serial.print("Sensor="); 
+    Serial.print(_message.data.id); 
+    Serial.print(", Value="); 
+    Serial.println(_message.data.value);
+#endif
+
+    // Send
+    vw_send(_message.raw, 5); // Send Raw message (size = 5 bytes)
+    vw_wait_tx(); // Wait until the whole message is gone
+  }
+
+  // sets the LED off
+  digitalWrite(txLed, LOW);   
+
+  // Power Off
+  powerDown();
+
+  // Go to Sleep Mode Zzzzz
+  deepSleep();
 }
 
 
@@ -51,7 +127,7 @@ boolean acquireApparentPower()
   boolean exit = false;
   boolean insideLine = false;
   char current;
-  char buffer[50];
+  char buffer[40];
 
   // Clear buffer
   buffer[0] = '\0';
@@ -88,22 +164,31 @@ boolean acquireApparentPower()
         // Turn Off Flag
         insideLine = false;
 
+#ifdef DEBUG
         //Serial.print("Data : <"); Serial.print(buffer); Serial.println(">");
+#endif
 
         char * papp;
-        papp = strstr (buffer,"PAPP");
+        papp = strstr(buffer,"PAPP");
 
-        if(papp!=NULL)
+        if (papp!=NULL)
         {
           // Remove CheckSum
           papp[10] = '\0';
-
-          Serial.print("PAPP = <"); Serial.print(papp); Serial.println(">");
-
+          
           // Convert to Float
           float value = atof(papp+5);
 
+#ifdef DEBUG
+          Serial.print("PAPP = <"); Serial.print(papp); Serial.println(">");
           Serial.print("PAPP = <"); Serial.print(value); Serial.println(">");
+#endif
+
+            // Save Power
+            _message.data.value = value;
+          
+            // Exit
+            return true;
         }
 
       }
@@ -119,8 +204,132 @@ boolean acquireApparentPower()
     }
   }
 
+  return false;
 }
 
+// 
+// Power Up Tx Module and sensor
+// Turn txLed ON
+void powerUp()
+{
+  // Power On
+  pinMode(powerPin, OUTPUT);
+  digitalWrite(powerPin,HIGH);
+  
+  delay(10);
+
+  // Led Setup
+  pinMode(txLed, OUTPUT);
+
+  // TX Setup
+  vw_set_tx_pin(txPin);
+  vw_setup(2000); // Transmition speed
+}
+
+//
+// Shut down power line
+void powerDown()
+{
+  // Power Down
+  digitalWrite(powerPin,LOW);
+
+  // Set all pin in INPUT mode
+  pinMode(powerPin, INPUT);
+  pinMode(txLed, INPUT);
+  pinMode(txPin, INPUT);
+}
+
+
+//
+// Put ATtiny85 in deep sleep mode 
+//
+// Power is reduce to :
+//    - ~7µA BOD disabled
+//    - ~30µA BOD enabled
+//
+void deepSleep()
+{
+#ifndef ECO
+  delay(8000*SLEEP_LOOP);
+#else
+  
+  //
+  // Prepare for shutdown
+  
+  bitClear(ADCSRA,ADEN); //Disable ADC
+  //bitSet(PRR,PRADC); not working...
+  //power_adc_disable(); not working...
+
+  ACSR = (1<<ACD); //Disable the analog comparator
+  DIDR0 = 0x3F; //Disable digital input buffers on all ADC0-ADC5 pins
+
+  PRR = 0x0F; //Reduce all power right before sleep
+  
+  
+  //
+  // Start Watchdog
+  setup_watchdog(WATCHDOG_TIMER);
+  
+  //
+  // Prepare for Sleep
+  
+  while(_watchdogCounter<SLEEP_LOOP){
+  
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // sleep mode
+  
+  noInterrupts();
+  sleep_enable();
+  // Disable BOD : my ATtiny85 doesn't have this fonctionality ...
+  interrupts();
+  
+  
+  sleep_cpu();                   // System sleeps here
+  sleep_disable();               // System continues execution here when watchdog timed out 
+  
+  } 
+  
+  //
+  // Stop WatchDog
+  setup_watchdog(-1);
+  
+  // Restore Power
+  PRR = 0x00; //Restore power reduction
+  DIDR0 = 0x00; //Restore digital input buffers on all ADC0-ADC5 pins
+#endif
+}
+
+
+#ifdef ECO
+
+//
+// Watchdog setup
+// From https://github.com/jcw/jeelib/blob/master/Ports.cpp
+// 
+void setup_watchdog(char mode) {
+
+  // correct for the fact that WDP3 is *not* in bit position 3!
+  if (mode & bit(3))
+  { 
+    mode ^= bit(3) | bit(WDP3);
+  }
+  // pre-calculate the WDTCSR value, can't do it inside the timed sequence
+  // we only generate interrupts, no reset
+  byte wdtcsr = mode >= 0 ? bit(WDIE) | mode : 0;
+  
+  MCUSR &= ~(1<<WDRF);
+
+  noInterrupts();
+
+  WDTCR |= (1<<WDCE) | (1<<WDE); // timed sequence
+  WDTCR = wdtcsr;
+  
+  // Reset counter
+  _watchdogCounter=0;
+  
+  interrupts();
+}
+
+#endif
 
 
 
