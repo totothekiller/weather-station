6c15188531ee40bd1bee60d4f223ea505046f07c
Thomas Pfeiffer <totothekiller@gmail.com>
2015-08-01 23:36:47 +0200

    Update Temp+Humidity Node

--- TempHumidityNode/TempHumidityNode.ino
+++ TempHumidityNode/TempHumidityNode.ino
@@ -4,8 +4,7 @@
 // Based on Sensor AM2302 (DHT22) http://snootlab.com/adafruit/252-capteur-de-temperature-et-humidite-am2302.html
 
 #include <Manchester.h>
-#include "DHT.h"
-#include <OneWire.h> // For CRC
+#include <DHT.h>
 #include <avr/wdt.h>
 #include <avr/power.h>
 #include <avr/interrupt.h>
@@ -35,7 +34,7 @@
 #endif
 
 #define WATCHDOG_TIMER WDTO_8S // 8 sec
-#define SLEEP_LOOP  2 // Deep Sleep total time = WATCHDOG_TIMER * SLEEP_LOOP
+#define SLEEP_LOOP  4 // Deep Sleep total time = WATCHDOG_TIMER * SLEEP_LOOP
 
 // Sensor Message Structure
 typedef struct sensorData_t{
@@ -61,6 +60,9 @@ ISR(WDT_vect) {
 }
 
 
+// Toggle flag : if true => Aquire Temp, if false => Acquire Humidity
+boolean _flag = true;
+
 // Initialize DHT sensor for normal 16mhz Arduino
 DHT dht(sensPin, DHTTYPE);
 
@@ -78,52 +80,28 @@ void loop()
   // Power On !
   powerUp();
 
+  // Turn LED On
+  digitalWrite(txLed, HIGH);
+
   // Read Temp
-  if (acquireTemp())
-  {  
-    // Turn LED On
-    digitalWrite(txLed, HIGH);
-        
-    // Compute Checksum : CRC8 on ID+Value
-    _message.data.checksum = OneWire::crc8(_message.raw, 5);
-    
-#ifdef DEBUG
-    Serial.print("Sensor="); 
-    Serial.print(_message.data.id); 
-    Serial.print(", Temp="); 
-    Serial.print(_message.data.value);
-    Serial.print(", CheckSum="); 
-    Serial.println(_message.data.checksum);
-#endif
-  
-    // Send All Data
-    man.transmitArray(6, _message.raw);
+  if (_flag && acquireTemp())
+  {
+    // Send !
+    fireNewValueEvent();
   }
-  
+ 
   // Read Humidity
-  if (acquireHumidity())
-  {  
-    // Turn LED On
-    digitalWrite(txLed, HIGH);
-        
-    // Compute Checksum : CRC8 on ID+Value
-    _message.data.checksum = OneWire::crc8(_message.raw, 5);
-    
-#ifdef DEBUG
-    Serial.print("Sensor="); 
-    Serial.print(_message.data.id); 
-    Serial.print(", Humidity="); 
-    Serial.print(_message.data.value);
-    Serial.print(", CheckSum="); 
-    Serial.println(_message.data.checksum);
-#endif
-  
-    // Send All Data
-    man.transmitArray(6, _message.raw);
+  if (!_flag && acquireHumidity())
+  {
+    // Send !
+    fireNewValueEvent();
   }
 
   // sets the LED off
-  digitalWrite(txLed, LOW);   
+  digitalWrite(txLed, LOW);
+
+  // Toggle flag
+  toggleFlag();
 
   // Power Off
   powerDown();
@@ -140,17 +118,16 @@ void powerUp()
   // Power On
   pinMode(powerPin, OUTPUT);
   digitalWrite(powerPin,HIGH);
-  
-  delay(10);
 
   // Led Setup
   pinMode(txLed, OUTPUT);
-
-  // TX Setup
-  man.setupTransmit(txPin);
+  
+  delay(500);
   
   // DHT Startup
   dht.begin();
+
+  delay(500);
 }
 
 //
@@ -214,6 +191,48 @@ boolean acquireHumidity()
   return true; 
 }
 
+//
+// Send New Sensor Value
+//
+void fireNewValueEvent()
+{
+  // Restart Watchdog
+  //wdt_reset();
+  
+  // Compute Checksum : CRC8 on ID+Value (Checksum is computed on the first 5 bytes)
+  _message.data.checksum = crc8(_message.raw, 5);
+
+#ifdef DEBUG
+    Serial.print("Sensor="); 
+    Serial.print(_message.data.id); 
+    Serial.print(", Value="); 
+    Serial.print(_message.data.value);
+    Serial.print(", CheckSum=0x"); 
+    Serial.println(_message.data.checksum, HEX);
+#endif
+
+  // TX Setup
+  man.setupTransmit(txPin);
+  
+  // Send All Data
+  man.transmitArray(6, _message.raw);
+}
+
+
+//
+// Toggle Boolean Flag
+//
+void toggleFlag()
+{
+  if (_flag)
+  {
+    _flag = false;
+  }
+  else
+  {
+    _flag = true;
+  }
+}
 
 //
 // Put ATtiny85 in deep sleep mode 
@@ -225,7 +244,7 @@ boolean acquireHumidity()
 void deepSleep()
 {
 #ifndef ECO
-  delay(8000*SLEEP_LOOP);
+  delay(120000); // 2 min sleep
 #else
   
   //
@@ -304,3 +323,40 @@ void setup_watchdog(char mode) {
 }
 
 #endif
+
+// This table comes from Dallas sample code where it is freely reusable,
+// though Copyright (C) 2000 Dallas Semiconductor Corporation
+static const uint8_t PROGMEM dscrc_table[] = {
+  0, 94,188,226, 97, 63,221,131,194,156,126, 32,163,253, 31, 65,
+  157,195, 33,127,252,162, 64, 30, 95,  1,227,189, 62, 96,130,220,
+  35,125,159,193, 66, 28,254,160,225,191, 93,  3,128,222, 60, 98,
+  190,224,  2, 92,223,129, 99, 61,124, 34,192,158, 29, 67,161,255,
+  70, 24,250,164, 39,121,155,197,132,218, 56,102,229,187, 89,  7,
+  219,133,103, 57,186,228,  6, 88, 25, 71,165,251,120, 38,196,154,
+  101, 59,217,135,  4, 90,184,230,167,249, 27, 69,198,152,122, 36,
+  248,166, 68, 26,153,199, 37,123, 58,100,134,216, 91,  5,231,185,
+  140,210, 48,110,237,179, 81, 15, 78, 16,242,172, 47,113,147,205,
+  17, 79,173,243,112, 46,204,146,211,141,111, 49,178,236, 14, 80,
+  175,241, 19, 77,206,144,114, 44,109, 51,209,143, 12, 82,176,238,
+  50,108,142,208, 83, 13,239,177,240,174, 76, 18,145,207, 45,115,
+  202,148,118, 40,171,245, 23, 73,  8, 86,180,234,105, 55,213,139,
+  87,  9,235,181, 54,104,138,212,149,203, 41,119,244,170, 72, 22,
+  233,183, 85, 11,136,214, 52,106, 43,117,151,201, 74, 20,246,168,
+  116, 42,200,150, 21, 75,169,247,182,232, 10, 84,215,137,107, 53};
+
+//
+// Compute a Dallas Semiconductor 8 bit CRC. These show up in the ROM
+// and the registers.  (note: this might better be done without to
+// table, it would probably be smaller and certainly fast enough
+// compared to all those delayMicrosecond() calls.  But I got
+// confused, so I use this table from the examples.)
+//
+uint8_t crc8(const uint8_t *addr, uint8_t len)
+{
+  uint8_t crc = 0;
+
+  while (len--) {
+    crc = pgm_read_byte(dscrc_table + (crc ^ *addr++));
+  }
+  return crc;
+}
